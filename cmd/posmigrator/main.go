62e532d4778e8a6585db168e8b0358cc02e9a597
Martin Munilla <martinmunillas@gmail.com>
2024-06-11 19:12:47 +0200

    feat: add validate flags

--- cmd/posmigrator/main.go
+++ cmd/posmigrator/main.go
@@ -10,10 +10,37 @@ import (
 
 var cfg posmigrator.Config
 
+func validateFlags(cfg posmigrator.Config) error {
+	if cfg.DbHost == "" {
+		return fmt.Errorf("dbhost is required")
+	}
+	if cfg.DbPort == "" {
+		return fmt.Errorf("dbport is required")
+	}
+	if cfg.DbUser == "" {
+		return fmt.Errorf("dbuser is required")
+	}
+	if cfg.DbPassword == "" {
+		return fmt.Errorf("dbpassword is required")
+	}
+	if cfg.DbName == "" {
+		return fmt.Errorf("dbname is required")
+	}
+	if cfg.MigrationsPath == "" {
+		return fmt.Errorf("migrationspath is required")
+	}
+	return nil
+}
+
 var migrateCmd = &cobra.Command{
 	Use:   "migrate",
 	Short: "Run migrations",
 	Run: func(cmd *cobra.Command, args []string) {
+		err := validateFlags(cfg)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		conn, err := posmigrator.ConnectPostgres(cfg)
 		if err != nil {
 			fmt.Println(err)
@@ -31,6 +58,11 @@ var ensureCmd = &cobra.Command{
 	Use:   "ensure",
 	Short: "Ensure all migrations ran and are valid",
 	Run: func(cmd *cobra.Command, args []string) {
+		err := validateFlags(cfg)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		conn, err := posmigrator.ConnectPostgres(cfg)
 		if err != nil {
 			fmt.Println(err)
